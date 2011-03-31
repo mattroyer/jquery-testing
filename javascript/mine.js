7faa3ba3357a8259b26fc5740b58f3287ae16c1b
Matt Royer <chuybakah@gmail.com>
2011-03-31 08:50:11 -0600

    Added project files and folders

---
+++ javascript/mine.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  // When "Powered" is clicked, it fades out and back in
+  // Writes "Animation compete!" in the .second class
+  $('.power').click(function() {
+    $('.power').fadeOut(500, function() {
+      $(this).show('slow');
+      $('p.second').replaceWith('<p class="second"><b>Animation complete!</b></p>');
+      $('p.second').fadeOut(1800);
+    });
+  });
+
+  // Fade in with the header
+  $('.header').fadeIn(3000);
+
+  // Prevent default click action on links
+  // Toggles which .nav section is hidden (Home/About links)
+  $('.nav a').click(function(event) {
+    event.preventDefault();
+    $('.nav').toggle();
+  });
+});
