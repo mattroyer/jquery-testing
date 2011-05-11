e58ec6c5076a64f0e28709f3a9c35c6b70393aea
Matt Royer <chuybakah@gmail.com>
2011-05-10 20:35:26 -0600

    Updated code, removed redundancies, cleaned up code, added functionality, removed other functionality.

--- javascript/mine.js
+++ javascript/mine.js
@@ -1,68 +1,105 @@
 $(document).ready(function() {
 
-  // When "Powered" is clicked, it fades out and back in
-  // Writes "Animation complete!" in the .second class
-  $('.power').click(function() {
-    $('.power').fadeOut(500, function() {
-      $(this).show('slow');
-      $('p.second').replaceWith('<p class="second"><b>Animation complete!</b></p>');
-      $('p.second').fadeOut(1800);
+  /* When "Powered" is clicked, it fades out and back in
+     Writes "Animation complete!" in the .second class */
+  $('#power').click(function() {
+    $('#power').fadeOut(500, function() {
+      $(this).fadeIn('slow', function() {
+        $('p.second').replaceWith('<p class="second"><b class="highlight">Animation complete!</b></p>');
+        $('p.second').fadeOut(1500);
+      });
     });
+    return false;
   });
 
-  // Fade in the header
-  $('.header').fadeIn(3000);
+  /* Hide the header first, then fade it in
+     This way there are no issues if javascipt
+     is turned off. Header still displays */
+  $('#header').css("display", "none").fadeIn(3000);
 
-  // Prevent default click action on nav links
-  // Toggles which .nav section is hidden (Home/About links)
+  /* Prevent default click action on nav links
+     Toggles which .nav section is hidden (Home/About links) */
   $('.nav a').click(function(event) {
     event.preventDefault();
-    $('.nav').toggle()
+    $('.nav').toggle();
+    return false;
   });
 
-  // Right-clicking the About link will take you
-  // to the about page
+  /* Right-clicking the About link will bring in
+     the about page content */
   $('.about').bind('contextmenu', function(event) {
     event.preventDefault();
-    window.location = $('.about').attr("href");
+    $('#indexContent').load('about.html #aboutContent1, #aboutContent2');
   });
 
 
-  // As long as the full window (document) has focus
-  // You'll be able to use this next bit of code
-  // I'll give you a hint:
-  // It's only useful on the About.html page
-  // And it's when you press a certain key (104)
+  /* As long as the full window (document) has focus
+     You'll be able to use this next bit of code
+     I'll give you a hint:
+     It's when you press a certain key (104) */
+
   $(this).keypress(function(event) {
     if (!$(event.target).is('input', 'textarea')) {
-      if (event.which == '104') {
+      if(event.which == '104') {
         window.location = $('.home').attr("href");
-      };
-    };
+      }
+    }
   });
 
   // ******************* //
   // YOURNAME CODE START //
   // ******************* //
 
-  // Click on the YourName link at the bottom
-  // of the page and it will bring up a text box
-  // Enter your name and upon submit (enter key)
-  // all yourName classes in the document will be
-  // changed to your value you input
+  /* Click on the YourName link at the bottom
+     of the page and it will bring up a text box
+     Enter your name and upon submit (enter key)
+     all yourName classes in the document will be
+     changed to your value you input */
+
+  var popupStatus = 0;
+
+  function loadPopup() {
+    if(popupStatus === 0) {
+      $("#popupBackground").css({"opacity": "0.7"});
+      $("#popupBackground").fadeIn("slow");
+      $("#popupForm").fadeIn("slow");
+      $("#name").focus();
+      popupStatus = 1;
+    }
+  }
+
+  function disablePopup() {
+    if(popupStatus === 1) {
+      $("#popupBackground").fadeOut("slow");
+      $("#popupForm").fadeOut("slow");
+      popupStatus = 0;
+    }
+  }
+
+  function centerPopup() {
+    var windowWidth = document.documentElement.clientWidth;
+    var windowHeight = document.documentElement.clientHeight;
+    var popupHeight = $("#popupForm").height();
+    var popupWidth = $("#popupForm").width();
+
+    $("#popupForm").css({"position": "absolute", "top": windowHeight/2-popupHeight/2, "left": windowWidth/2-popupWidth/2 });
+    $("#popupBackground").css({"height": windowHeight });
+  }
 
   // The click function to bring up a text box
   $('#yourNameLink').click(function(nameEvent) {
     nameEvent.preventDefault();
-    $(this).hide();
     $('#nameForm').show();
+    centerPopup();
+    loadPopup();
+    $('#name').val('');
   });
 
   // The ajax submit function
   $('#nameForm').submit(function(e) {
     e.preventDefault();
    
-    var name = $('#name').val()
+    var name = $('#name').val();
 
     $.ajax({
       data: name,
@@ -70,46 +107,66 @@ $(document).ready(function() {
         $('#nameForm').hide();
         $('.yourName').text(name);
         $('#yourNameLink').show();
+        disablePopup();
       }
     });
     return false;
+  });
+
+  $("#popupFormClose").click(function() {
+    disablePopup();
+  });
+  $("#popupBackground").click(function() {
+    disablePopup();
+  });
+  $(document).keypress(function(escKey) {
+    if(escKey.keyCode === 27 && popupStatus === 1) {
+      disablePopup();
+    }
   });
 
   // ***************** //
   // YOURNAME CODE END //
   // ***************** //
 
-  // ********************* //
-  // SUBHEADING CODE START //
-  // ********************* //
+  // ***************** //
+  // TYPEIT CODE START //
+  // ***************** //
 
-  // Get the text from the subHeading class
-  // Split it and turn it into an array
-  // Store in the variable "letter"
-  var letter = $('.subHeading').text().split('');
+  var letter = 0;
+  var phrase = "";
+  var line = "";
+  var running = false;
 
-  // Get each letter element and attach a span class to it
-  $(letter).each(function(i) {
-    $('.subHeading').append('<span class="' + letter[i] + '">' + letter[i] + '</span>')
+  function showPhrase(obj) {
+    phrase = obj.text();
+    if(phrase) {
+      typeIt();
+    }
+  }
+
+  function typeIt() {
+    $(line).text(phrase.substr(0, letter++));
+    if(letter <= phrase.length) {
+      setTimeout(typeIt, 75);
+    } else {
+      letter = 0;
+      phrase = "";
+      running = false;
+    }
+  }
+
+  $('#indexContent p').click(function(clickedLine) {
+    if(!running) {
+      running = true;
+      line = $(clickedLine.currentTarget);
+      $(line).height($(line).height());
+      showPhrase(line);
+    }
+    return false;
   });
 
-  // Remove the initial phrase that was split into an array
-  $('.change').remove();
-
-  // Function to show each letter one at a time
-  $('.subHeading span:first-child').show("fast", function() {
-    $(this).next().show("fast", arguments.callee);
-  });
-
-  // Function to hide each letter one at a time when clicked
-  $('.subHeading').click(function() {
-    $('.subHeading span:last-child').fadeOut("fast", function() {
-      $(this).prev().fadeOut(100, arguments.callee);
-    });
-    $(this).append('&nbsp;');
-  });
-
-  // ******************* //
-  // SUBHEADING CODE END //
-  // ******************* //
+  // *************** //
+  // TYPEIT CODE END //
+  // *************** //
 });
